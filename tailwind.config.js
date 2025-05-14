deecf7fd921432e00b004521717273dd461e712f
johannes debritto <johanesdebrito22@gmail.com>
2025-05-14 14:35:28 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // Menangkap semua file di folder pages
+    "./components/**/*.{js,ts,jsx,tsx}", // Menangkap semua file di folder components
+    "./app/**/*.{js,ts,jsx,tsx}", // Jika kamu menggunakan App directory (Next.js 13)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
